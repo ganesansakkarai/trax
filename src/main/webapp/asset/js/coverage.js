1ab1a9e8087915c4b726b0a1f36c31cddba76f3e
Saravana Aravind Rajaratnam <saravana.aravind@yahoo.com>
2014-04-03 19:46:07 -0400

    fixed coverage

--- src/main/webapp/asset/js/coverage.js
+++ src/main/webapp/asset/js/coverage.js
@@ -2,7 +2,6 @@ $(document).ready(function() {
     oTable = $('#coverage').dataTable({
         "bJQueryUI": true,
         "sPaginationType": "full_numbers",
-        "bProcessing": true,
         "bServerSide": true,
         "sAjaxSource": "http://traxtest.herokuapp.com/coverages/",
         "sServerMethod": "POST",
