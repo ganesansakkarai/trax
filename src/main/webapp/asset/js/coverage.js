545cc99fcf8db18c368000cbcd131f78b1c7cd6e
Saravana Aravind Rajaratnam <saravana.aravind@yahoo.com>
2014-04-04 21:44:17 -0400

    column centered

--- src/main/webapp/asset/js/coverage.js
+++ src/main/webapp/asset/js/coverage.js
@@ -1,17 +1,26 @@
 $(document).ready(function() {
-    oTable = $('#coverage').dataTable({
-        "bJQueryUI": true,
-        "sPaginationType": "full_numbers",
-        "bServerSide": true,
-        "sAjaxSource": "http://traxtest.herokuapp.com/coverages/",
-        "sServerMethod": "POST",
-        "aoColumns": [
-            { "mData": "name" },
-            { "mData": "timestamp" },
-            { "mData": "lines" },
-            { "mData": "missedLines" },
-            { "mData": "branch" },
-            { "mData": "missedBranch" },
-        ]
-    });
-} );
+	oTable = $('#coverage').dataTable({
+		"bJQueryUI" : true,
+		"sPaginationType" : "full_numbers",
+		"bServerSide" : true,
+		"sAjaxSource" : "http://traxtest.herokuapp.com/coverages/",
+		"sServerMethod" : "POST",
+		"aoColumns" : [ {
+			"mData" : "name"
+		}, {
+			"mData" : "timestamp"
+		}, {
+			"mData" : "lines"
+		}, {
+			"mData" : "missedLines"
+		}, {
+			"mData" : "branch"
+		}, {
+			"mData" : "missedBranch"
+		}, ],
+		"aoColumnDefs" : [ {			
+			"sClass" : "center",
+			"aTargets" : [ 1,2,3,4,5 ]
+		} ]
+	});
+});
