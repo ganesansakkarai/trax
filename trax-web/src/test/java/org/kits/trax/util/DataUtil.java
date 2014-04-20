f3b6a015f22b96e1fd130ac868e0a4f1629b9189
unknown <ganeshciet2006@gmail.com>
2014-04-20 15:33:36 +0530

    added data for test results

--- trax-web/src/test/java/org/kits/trax/util/DataUtil.java
+++ trax-web/src/test/java/org/kits/trax/util/DataUtil.java
@@ -6,9 +6,13 @@ import org.kits.trax.domain.Application;
 import org.kits.trax.domain.Clazz;
 import org.kits.trax.domain.Method;
 import org.kits.trax.domain.Module;
+import org.kits.trax.domain.TestCase;
+import org.kits.trax.domain.TestStatus;
+import org.kits.trax.domain.TestSuite;
 import org.kits.trax.domain.TestType;
 
 public class DataUtil {
+	
 
 	public static Application build() {
 
@@ -34,6 +38,37 @@ public class DataUtil {
 			method.setMissedBranch(4);
 			clazz.getMethods().add(method);
 		}
+		
+		
+		module.setDuration(5l);
+		module.setEndTime(new Date());
+		module.setStartTime(new Date());
+		module.setFail(10l);
+		module.setName("Sample");
+		module.setPass(10l);
+		module.setSkip(5l);
+		module.setSuccess(10l);
+		
+		TestSuite testSuite = new TestSuite();
+		testSuite.setDuration(5l);
+		testSuite.setEndTime(new Date());
+		testSuite.setStartTime(new Date());
+		testSuite.setFail(10l);
+		testSuite.setName("Sample");
+		testSuite.setPass(10l);
+		testSuite.setSkip(5l);
+		testSuite.setSuccess(10l);
+		module.getTestSuites().add(testSuite);
+		
+		for (int i = 0; i < 10; ++i) {
+			TestCase testCase = new TestCase();
+			testCase.setName("test" + i);
+			testCase.setDuration(10l);
+			testCase.setLog("testing");
+			testCase.setStatus(TestStatus.PASS);
+			testSuite.getTestCases().add(testCase);
+		}
+		
 
 		return app;
 	}
